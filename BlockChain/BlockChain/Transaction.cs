90ea9ffff7851ff2ca0ef553a6c13ab8483e4929
Vishal Chaniyara <vishalchaniyara22@gmail.com>
2020-06-22 21:20:45 -0700

    Add files via upload
    Blockchain using C# console app

---
+++ BlockChain/BlockChain/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockChain
+{
+    public class Transaction
+    {
+        public string From { get; }
+        public string To { get; }
+        public double Amount { get; }
+        public Transaction(string from, string to, double amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
